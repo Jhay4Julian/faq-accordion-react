a841a4276e6bc4967c9d1817a735d96b7a12fd25
Julian Koripamo <jkoripamo@gmail.com>
2025-05-02 13:57:03 +0100

    implemented components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css'
+import Header from './components/Header'
+import Faq from './components/Faq'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className='bg-purple-100 min-h-screen'>
+      <div className='relative'>
+        <Header />
+        <div className='absolute top-30 left-0 w-full h-full'>
+          <Faq />
+        </div>
+      </div>
+    </div>
   )
 }
 
